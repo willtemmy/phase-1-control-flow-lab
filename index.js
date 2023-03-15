f02dc0cb0e90104d91a33a1b4b2268687e485c37
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-15 16:10:54 +0300

    Completed

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let message
+  if(distance<=400){
+    message = "This one is on me!";
+  }else if(distance>2000 && distance<=2500){
+    message = "I will gladly take your thirty bucks.";
+  }else if(distance>2500){
+    message = "No can do.";
+  }
+
+  return message;
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  const newc = "NYC";
+  let mess
+  if(city === newc){
+    mess = "Ok, sounds good.";
+  }else{
+    mess = "No go.";
+  }
+
+  return mess;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let thanks
+  if (tip === 'generous') {
+    thanks = "Thank you so much.";
+  }else if (tip === 'not as generous') {
+    thanks = 'Thank you.';
+  }else{
+    thanks = "Bye.";
+  }
+
+  return thanks;
 }
